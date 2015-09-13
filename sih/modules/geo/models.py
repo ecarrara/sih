3b8b6e66cd57efcaec306b9836148356e9a070f9
Erle Carrara <carrara.erle@gmail.com>
2015-09-13 12:10:20 -0300

    adiciona lista de estados brasileiros

--- sih/modules/geo/models.py
+++ sih/modules/geo/models.py
@@ -10,6 +10,36 @@ from geoalchemy2.types import Geography
 from sih.extensions import db
 
 
+STATES = (
+    ('AC', u'Acre'),
+    ('AL', u'Alagoas'),
+    ('AP', u'Amapá'),
+    ('AM', u'Amazonas'),
+    ('BA', u'Bahia'),
+    ('CE', u'Ceará'),
+    ('DF', u'Distrito Federal'),
+    ('ES', u'Espirito Santo'),
+    ('GO', u'Goiás'),
+    ('MA', u'Maranhão'),
+    ('MT', u'Mato Grosso'),
+    ('MS', u'Mato Grosso do Sul'),
+    ('MG', u'Minas Gerais'),
+    ('PA', u'Pará'),
+    ('PB', u'Paraíba'),
+    ('PR', u'Paraná'),
+    ('PE', u'Pernambuco'),
+    ('PI', u'Piauí'),
+    ('RJ', u'Rio de Janeiro'),
+    ('RN', u'Rio Grande do Norte'),
+    ('RS', u'Rio Grande do Sul'),
+    ('RO', u'Rondônia'),
+    ('SC', u'Santa Catarina'),
+    ('SP', u'São Paulo'),
+    ('SE', u'Sergipe'),
+    ('TO', u'Tocantins')
+)
+
+
 class City(db.Model):
 
     __tablename__ = 'cities'
