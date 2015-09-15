8bcb709710be7135f3f0420561bdf5ad94cd8ebe
Erle Carrara <carrara.erle@gmail.com>
2015-09-15 19:31:53 -0300

    cria o módulo data

---
+++ sih/modules/data/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    sih.modules.data
+    ~~~~~~~~~~~~~~~~
+
+    :copyright: (c) 2015 by Erle Carrara.
+"""
+
+from flask import Blueprint
+
+
+data = Blueprint('data', __name__, template_folder='templates')
