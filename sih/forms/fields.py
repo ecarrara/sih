1313a3b65096257663bd4e3a412393fb20ba3ff3
Erle Carrara <carrara.erle@gmail.com>
2015-09-15 19:16:40 -0300

    py3: unicode -> six.text_type :(

--- sih/forms/fields.py
+++ sih/forms/fields.py
@@ -6,6 +6,7 @@
     :copyright: (c) 2015 by Erle Carrara.
 """
 
+from six import text_type
 from wtforms.fields import TextField
 
 
@@ -13,7 +14,7 @@ class ListField(TextField):
 
     def process_formdata(self, valuelist):
         if valuelist:
-            self.data = map(unicode.strip, valuelist[0].split(u','))
+            self.data = map(text_type.strip, valuelist[0].split(u','))
 
     def _value(self):
         return u', '.join(self.data) if self.data else ''
