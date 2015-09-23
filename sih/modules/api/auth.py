5fa893d30dd932b63133d3ab1446e5dade1d8b44
Erle Carrara <carrara.erle@gmail.com>
2015-09-22 22:36:17 -0300

    py3 fix: base64.b64encode expect bytes

--- sih/modules/api/auth.py
+++ sih/modules/api/auth.py
@@ -10,4 +10,5 @@ from base64 import b64encode
 
 
 def basic_auth(username, password):
-    return u'Basic {}'.format(b64encode('{}:{}'.format(username, password)))
+    auth = '{}:{}'.format(username, password)
+    return 'Basic {}'.format(b64encode(auth.encode('utf-8')).decode('utf-8'))
